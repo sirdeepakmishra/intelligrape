342f69fe1688c4be1d4cf69cf664571695e1c916
deepak <sirdeepakmishra@gmail.com>
2015-03-26 22:32:14 +0530

    this is firstdeepak.java

---
+++ deepak.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class Deepak
+{
+public void heheh()
+{
+for(int i=0; i<5;i++)
+System.out.println("cdac");
+}
+public static void main(String[] args)
+{
+deepak obj=new deepak();
+obj.heheh();
+System.out.print("hello");
+System.out.println("hello");
+System.out.print("hello");
+System.out.println("hello");
+}
+}
